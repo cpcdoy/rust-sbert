18a5424b899eb2b1cbbad06b1b2109e7348a5348
Chady <chady.dimachkie@ubisoft.com>
2020-12-21 19:05:35 +0100

    Add: CharacterBertModel, CharacterBertEmbeddings, CharacterCNN and Highway layer implementations

--- src/models/character_bert.rs
+++ src/models/character_bert.rs
@@ -1,11 +1,10 @@
 use std::borrow::Borrow;
 use std::collections::HashMap;
 
-use rust_bert::bert::BertEncoder;
-use rust_bert::bert::BertPooler;
-use rust_bert::bert::{BertConfig};
-use rust_bert::{Config, RustBertError};
+use rust_bert::bert::{BertConfig, BertEncoder, BertEncoderOutput, BertPooler};
+use rust_bert::{Activation, Config, RustBertError};
 use serde::{Deserialize, Serialize};
+use tch::nn::Module;
 use tch::{nn, Kind, Tensor};
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -59,16 +58,286 @@ impl CharacterBertConfig {
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
+pub struct Highway {
+    layers: Vec<nn::Linear>,
+    activation: Activation,
+}
+
+impl Highway {
+    pub fn new<'p, P>(p: P, input_dim: i64, num_layers: i64, activation: Activation) -> Self
+    where
+        P: Borrow<nn::Path<'p>>,
+    {
+        let p = p.borrow() / "_layers";
+        let mut layers = Vec::<nn::Linear>::with_capacity(num_layers as usize);
+        for i in 0..num_layers {
+            let lin_conf = nn::LinearConfig::default();
+            let linear = nn::linear(
+                p.borrow() / i.to_string().as_str(),
+                input_dim,
+                input_dim * 2,
+                lin_conf,
+            );
+
+            layers.push(linear);
+        }
+
+        Self { layers, activation }
+    }
+
+    pub fn forward(&self, inputs: Tensor) -> Result<Tensor, RustBertError> {
+        let mut current_input = inputs;
+        for layer in &self.layers {
+            let projected_input = layer.forward(&current_input);
+            let linear_part = current_input.shallow_clone();
+
+            let linear_part_chunk = projected_input.chunk(2, -1);
+            let (nonlinear_part, gate) = (
+                linear_part_chunk[0].shallow_clone(),
+                linear_part_chunk[1].shallow_clone(),
+            );
+            let nonlinear_part = match self.activation {
+                Activation::relu => nonlinear_part.relu(),
+                Activation::tanh => nonlinear_part.tanh(),
+                _ => nonlinear_part.relu(),
+            };
+
+            let gate = gate.sigmoid();
+            current_input = gate.shallow_clone() * linear_part + (1 - gate) * nonlinear_part;
+        }
+
+        Ok(current_input)
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct CharacterCNNOptions {
+    activation: Activation,
+    filters: [[i64; 2]; 7],
+    n_highways: i64,
+    embedding_dim: i64,
+    n_characters: i64,
+    max_characters_per_token: i64,
+}
+
+pub struct CharacterCNN {
+    options: CharacterCNNOptions,
+    char_embeddings: nn::Embedding,
+    convolutions: Vec<nn::Conv1D>,
+    highway: Highway,
+    projection: nn::Linear,
+    //
+    // Defined in the original CharacterCNN but not here
+    //requires_grad: bool,
+    //beginning_of_sentence_characters: Tensor
+    //end_of_sentence_characters: Tensor,
+}
+
+impl CharacterCNN {
+    pub fn new<'p, P>(p: P, conf: &CharacterBertConfig) -> Self
+    where
+        P: Borrow<nn::Path<'p>>,
+    {
+        let options = CharacterCNNOptions {
+            activation: Activation::relu,
+            filters: [
+                [1, 32],
+                [2, 32],
+                [3, 64],
+                [4, 128],
+                [5, 256],
+                [6, 512],
+                [7, 1024],
+            ],
+            n_highways: 2,
+            embedding_dim: 16,
+            n_characters: 65536,
+            max_characters_per_token: 50,
+        };
+
+        //let output_dim = conf.hidden_size;
+
+        // Init char embeddings
+        let emb_conf = nn::EmbeddingConfig::default();
+        let char_embeddings = nn::embedding(
+            p.borrow() / "_char_embedding_weights",
+            options.n_characters + 1,
+            options.embedding_dim,
+            emb_conf,
+        );
+
+        // Init CNNs weights
+        let mut convolutions = Vec::<nn::Conv1D>::with_capacity(options.filters.len());
+        for (i, [width, num]) in options.filters.iter().enumerate() {
+            let mut conv_conf = nn::ConvConfig::default();
+            conv_conf.bias = true;
+
+            let conv = nn::conv1d(
+                p.borrow() / format!("char_conv_{}", i).as_str(),
+                options.embedding_dim,
+                *num,
+                *width,
+                conv_conf,
+            );
+
+            convolutions.push(conv);
+        }
+
+        // Init highway
+        let n_filters = options.filters.iter().fold(0, |sum, i| sum + i[1]);
+        let highway = Highway::new(
+            p.borrow() / "_highways",
+            n_filters,
+            options.n_highways,
+            Activation::relu,
+        );
+
+        // Init projection
+        let lin_conf = nn::LinearConfig::default();
+        let projection = nn::linear(
+            p.borrow() / "_projection",
+            n_filters,
+            conf.hidden_size,
+            lin_conf,
+        );
+
+        Self {
+            options,
+            char_embeddings,
+            convolutions,
+            highway,
+            projection,
+        }
+    }
+
+    pub fn forward(&self, inputs: Tensor) -> Result<Tensor, RustBertError> {
+        let mask = (inputs.ge(0))
+            .to_kind(Kind::Int64)
+            .sum1(&[-1], false, Kind::Int64);
+
+        let (character_ids_with_bos_eos, _mask_with_bos_eos) = (inputs, mask);
+
+        let max_chars_per_token = self.options.max_characters_per_token;
+
+        let character_embedding = self
+            .char_embeddings
+            .forward(&character_ids_with_bos_eos.view([-1, max_chars_per_token]));
+
+        let character_embedding = character_embedding.transpose(1, 2);
+        let mut convs = Vec::<Tensor>::with_capacity(self.convolutions.len());
+        for conv in self.convolutions.iter() {
+            let convolved = conv.forward(&character_embedding);
+
+            let (convolved, _) = convolved.max2(-1, false);
+            let convolved = match self.options.activation {
+                Activation::relu => convolved.relu(),
+                Activation::tanh => convolved.tanh(),
+                _ => convolved.relu(),
+            };
+
+            convs.push(convolved);
+        }
+
+        let mut token_embedding = Tensor::cat(&convs, -1);
+        token_embedding = self.highway.forward(token_embedding).unwrap();
+        token_embedding = self.projection.forward(&token_embedding);
+
+        let (batch_size, sequence_length) = (
+            character_ids_with_bos_eos.size()[0],
+            character_ids_with_bos_eos.size()[1],
+        );
+
+        Ok(token_embedding.view([batch_size, sequence_length, -1]))
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 pub struct BertCharacterEmbeddings {
     word_embeddings: CharacterCNN,
     position_embeddings: nn::Embedding,
     token_type_embeddings: nn::Embedding,
     layer_norm: nn::LayerNorm,
-    dropout: u32,
+    hidden_dropout_prob: f64,
 }
 
 impl BertCharacterEmbeddings {
-    
+    pub fn new<'p, P>(p: P, conf: &CharacterBertConfig) -> Self
+    where
+        P: Borrow<nn::Path<'p>>,
+    {
+        let p = p.borrow();
+        let CharacterBertConfig {
+            hidden_size,
+            max_position_embeddings,
+            type_vocab_size,
+            hidden_dropout_prob,
+            ..
+        } = *conf;
+
+        let word_embeddings = CharacterCNN::new(p / "word_embeddings", conf);
+
+        let emb_conf = nn::EmbeddingConfig::default();
+        let position_embeddings = nn::embedding(
+            p / "position_embeddings",
+            max_position_embeddings,
+            hidden_size,
+            emb_conf,
+        );
+
+        let emb_conf = nn::EmbeddingConfig::default();
+        let token_type_embeddings = nn::embedding(
+            p / "token_type_embeddings",
+            type_vocab_size,
+            hidden_size,
+            emb_conf,
+        );
+
+        let mut layer_norm_conf = nn::LayerNormConfig::default();
+        layer_norm_conf.eps = 1e-12;
+
+        let layer_norm =
+            nn::layer_norm(p / "LayerNorm", vec![hidden_size], layer_norm_conf.clone());
+
+        Self {
+            word_embeddings,
+            position_embeddings,
+            token_type_embeddings,
+            layer_norm,
+            hidden_dropout_prob,
+        }
+    }
+
+    pub fn forward_t(
+        &self,
+        input_ids: Tensor,
+        token_type_ids: Tensor,
+        position_ids: Option<Tensor>,
+        train: bool,
+    ) -> Result<Tensor, RustBertError> {
+        let seq_length = input_ids.size()[1];
+
+        let position_ids = position_ids.unwrap_or_else(|| {
+            let position_ids = Tensor::arange(seq_length, (Kind::Int64, input_ids.device()));
+            position_ids
+                .unsqueeze(0)
+                .expand_as(&input_ids.slice(2, 0, 1, 1))
+        });
+
+        // In original implem, but redundant...
+        // let token_type_ids =
+        //     token_type_ids.unwrap_or_else(|| input_ids.slice(2, 0, 1, 1).zeros_like());
+
+        let word_embeddings = self.word_embeddings.forward(input_ids).unwrap();
+        let position_embeddings = self.position_embeddings.forward(&position_ids);
+        let token_type_embeddings = self.token_type_embeddings.forward(&token_type_ids);
+        let mut embeddings = word_embeddings + position_embeddings + token_type_embeddings;
+        embeddings = self.layer_norm.forward(&embeddings);
+        embeddings = embeddings.dropout(self.hidden_dropout_prob, train);
+
+        Ok(embeddings)
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -99,7 +368,7 @@ impl CharacterBertModel {
         let is_decoder = conf.is_decoder.unwrap_or(false);
 
         let embeddings = BertCharacterEmbeddings::new(&p / "embeddings", conf);
-        let encoder = BertEncoder::new(&p / "encoder", conf);
+        let encoder = BertEncoder::new(&p / "encoder", &bert_conf);
         let pooler = BertPooler::new(&p / "poolr", &bert_conf);
 
         CharacterBertModel {
@@ -125,7 +394,10 @@ impl CharacterBertModel {
             (Some(_), Some(_)) => Err(RustBertError::ValueError(
                 "You cannot specify both input_ds and inputs_embeds at the same time".into(),
             ))?,
-            (Some(input_value), None) => (input_value.size(), input_value.device()),
+            (Some(input_value), None) => (
+                vec![input_value.size()[0], input_value.size()[1]],
+                input_value.device(),
+            ),
             (None, Some(embeds)) => (vec![embeds.size()[0], embeds.size()[1]], embeds.device()),
             (None, None) => Err(RustBertError::ValueError(
                 "You have to specify either input_ids or inputs_embeds".into(),
@@ -148,7 +420,7 @@ impl CharacterBertModel {
                         .unsqueeze(0)
                         .repeat(&[batch_size, seq_length, 1]);
                     let causal_mask = causal_mask.le1(&seq_ids.unsqueeze(0).unsqueeze(-1));
-                    causal_mask = causal_mask.to_kind(attention_mask.kind());
+                    let causal_mask = causal_mask.to_kind(attention_mask.kind());
 
                     causal_mask * attention_mask.unsqueeze(1).unsqueeze(2)
                 } else {
@@ -201,14 +473,16 @@ impl CharacterBertModel {
                 None
             };
 
+        let input_ids = input_ids.unwrap();
+
         let embedding_output =
             self.embeddings
-                .forward_t(input_ids, position_ids, token_type_ids)?;
+                .forward_t(input_ids, token_type_ids, position_ids, train)?;
 
-        let CharacterBertEncoderOutput {
-            sequence_output,
-            hidden_states,
-            attentions,
+        let BertEncoderOutput {
+            hidden_state,
+            all_hidden_states,
+            all_attentions,
         } = self.encoder.forward_t(
             &embedding_output,
             &Some(extended_attention_mask),
@@ -216,6 +490,10 @@ impl CharacterBertModel {
             &encoder_extended_attention_mask,
             train,
         );
+
+        let sequence_output = hidden_state;
+        let hidden_states = all_hidden_states;
+        let attentions = all_attentions;
 
         let pooled_output = self.pooler.forward(&sequence_output);
 
